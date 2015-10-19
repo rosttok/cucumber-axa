716ffc85f99d1a5ae0364f97dc91b0431d2513dc
rafa.hidalgo <rahife@users.noreply.github.com>
2015-10-19 22:10:01 +0700

    Create one project OK

--- env-scripts/create-env.sh
+++ env-scripts/create-env.sh
@@ -15,6 +15,6 @@ NUM_RESOURCES=5
 NUM_PROJECTS=5
 URL='http://52.17.174.169:5000'
 
-. ./create-competencies.sh $NUM_COMPETENCIES $URL
+./create-staffing-groups.sh $NUM_STAFFING_GROUPS $URL | xargs -I {} ./create-resources.sh $NUM_RESOURCES {} $URL
 
-. ./create-staffing-groups.sh 10 http://52.17.174.169:5000 | xargs -0 -I {} ./create-resources.sh 10 "c2146205-6907-4747-8725-f7dacd729e8c" http://52.17.174.169:5000
+./create-competencies.sh $NUM_COMPETENCIES $URL
---
+++ env-scripts/create-project.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#Create Project:
+#WIP
+#$1: OWNER_ID
+#$2: URL > ex: http://52.17.174.169:5000
+
+set -e
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+URL="$2/api/project"
+echo "Creating Project at "$URL
+
+JSON1='{"project": {"name": "Project'
+JSON2='","wbs": "WBS'
+JSON3='","description": "a project","responsible": "'
+JSON4='",
+"startDate": "2015-03-01T17:00:00.000Z",
+"endDate": "2015-05-01T17:00:00.000Z"
+}
+}'
+
+curl -X POST -H "Content-Type: application/json" -d "$JSON1$(date +"%N")$JSON2$(date +"%N")$JSON3$1$JSON4" $URL | jq .
+
+echo ""
